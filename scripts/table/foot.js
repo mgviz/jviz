d6e5e9605021e07ce3a98a1cb2a8bf604c163030
jmjuanes <josemijuanes@gmail.com>
2016-02-26 16:11:34 +0100

    Update foot events

--- scripts/table/foot.js
+++ scripts/table/foot.js
@@ -14,11 +14,11 @@ jvizTable.prototype.FootRowsChange = function()
 //jvizTable show page in foot
 jvizTable.prototype.FootShowPage = function()
 {
-  //Calculate the real page
-  var realp = this.page.now + 1;
+  //Show page now
+  $('#' + this.foot.page.input.id).val(this.page.now + 1);
 
-  //Show page
-  $('#' + this.foot.page.total.id).text('Page ' + realp + ' of ' + this.page.num);
+  //Show total pages
+  $('#' + this.foot.page.total.id).html(this.page.num);
 };
 
 //jvizTable show total items
@@ -33,24 +33,17 @@ jvizTable.prototype.FootEvnt = function()
 {
   //Call the function for the buttons
   jvizTableFootBtnEvnt(this);
-
-  //Call the function for the select rows
-  jvizTableFootRowsEvnt(this);
 };
 
-//jvizTable foot click event */
-function jvizTableFootBtnEvnt(_main)
+//jvizTable foot click event
+function jvizTableFootEvent(_main)
 {
   //Create the event for the prev button
-  $('#' + _main.footbtn.prev).on('click', function(e){ _main.PageRev(); });
+  $('#' + _main.foot.btn.prev.id).on('click', function(e){ _main.PageRev(); });
 
   //Create the event for the next button
-  $('#' + _main.footbtn.next).on('click', function(e){ _main.PageNext(); });
-}
+  $('#' + _main.foot.btn.next.id).on('click', function(e){ _main.PageNext(); });
 
-//jvizTable foot Rows select event
-function jvizTableFootRowsEvnt(_main)
-{
   //Create the event for change the select value
   $('#' + _main.footrows.select.id).on('change', function(e){ _main.FootRowsChange(); });
 }
