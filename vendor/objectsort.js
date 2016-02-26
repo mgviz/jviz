d6eb5f97ffdcc13139bda28eecd37b926eee0602
jmjuanes <josemijuanes@gmail.com>
2016-02-26 15:27:09 +0100

    Update vendor libs

--- vendor/objectsort.js
+++ vendor/objectsort.js
@@ -1,58 +1,90 @@
 //Sort main function
-function ObjectSort(array, by)
+function ObjectSort(array, columns, order)
 {
-	//Array with the selected cols
-	var newarr = [];
-	
-	//Get only
-	for(var i = 0; i < array.length; i++)
+	//Check the columns
+	if(typeof columns === 'undefined')
 	{
-		//Get
-		newarr.push(array[i][by]);
+		//Create the columns array
+		var columns = [];
+
+		//Add the keys
+		for(var key in array[0]){ columns.push(key); }
 	}
-	
-	//Sort
-	var index = ArraySort(newarr);
-	
-	//Restart the array
-	newarr = [];
-	
-	//Loop for sort the original array with the new indices
-	for(var i = 0; i < index.length; i++)
+
+	//Check if columns is not an array
+	else if(Array.isArray(columns) === false)
 	{
-		newarr.push(array[index[i]]);
+		//Convert it to array
+		columns = [columns];
 	}
-	
-	//Return the sorted array
-	return newarr;
+
+	//Check the order
+	if(typeof order === 'undefined')
+	{
+		//Create the order array
+		var order = [];
+
+		//Add ASC order
+		for(var i = 0; i < columns.length; i++){ order.push('ASC'); }
+	}
+
+	//Check if order is not an array
+	else if(Array.isArray(order) === false)
+	{
+		//Convert it to array
+		order = [order];
+	}
+
+	//Else, check for lowercase
+	else
+	{
+		//Add the order to uppercase
+		for(var i = 0; i < order.length; i++){ order[i] = order[i].toUpperCase(); }
+	}
+
+	//Check the order array length
+	if(order.length < columns.length)
+	{
+		//Complete the order array
+		for(var i = order.length; i < columns.length; i++){ order.push('ASC'); }
+	}
+
+	//Sort the array
+	array.sort(function(left, right){ return Compare(left, right, columns, order); });
+
+	//Return the array
+	return array;
 }
 
-
-//Sort an array
-function ArraySort(array)
+//Function for compare two elements
+function Compare(left, right, columns, order)
 {
-	//Index arrays
-	var index = [];
-	
-	//Add the elements with the index
-	for(var k = 0; k < array.length; k++)
+	//Compare all
+	for(var i = 0; i < columns.length; i++)
 	{
-		array[k] = [array[k], k];
-	}
-	
-	//Sort the array
-	array.sort(function(left, right){ return left[0] < right[0] ? -1 : 1; });
-	
-	//Add the index
-	for(var k = 0; k < array.length; k++)
-	{
-		index.push(array[k][1]);
-	}
-	
-	//Return the indices
-	return index;
-};
+		//Check if que difference is numeric
+		var numeric = !isNaN(+left[columns[i]] - +right[columns[i]]);
 
+		//Get the values
+		var a = (numeric === true) ? +left[columns[i]] : left[columns[i]].toLowerCase();
+		var b = (numeric === true) ? +right[columns[i]] : right[columns[i]].toLowerCase();
+
+		//Check the values
+		if(a < b)
+		{
+			//Check the order
+			return (order[i] === 'ASC') ? -1 : 1;
+		}
+		else if(a > b)
+		{
+			//Check the order
+			return (order[i] === 'ASC') ? 1 : -1;
+		}
+	}
+
+	//Default, return 0
+	return 0;
+}
 
 //Check for exports
 if (typeof module === "object" && module.exports)
