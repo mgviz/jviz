19ea963b1a89d6ea6ea76848757c4bda0ad4ace2
jmjuanes <josemijuanes@gmail.com>
2016-03-06 13:55:10 +0100

    Update cover track

--- scripts/coverviewer/tracks/cover.js
+++ scripts/coverviewer/tracks/cover.js
@@ -8,18 +8,11 @@ CoverViewer.prototype.CoverTrack = function()
 CoverViewer.prototype.CoverTrackBarRegionInfo = function()
 {
   //Generate the region
-  var reg = ' ' + this.cover.start + '-' + this.cover.end;
+  var reg = ' ' + this.cover.draw.start + '-' + this.cover.draw.end;
 
   //Show on the bar
   this.cover.SetTitle(this.cover.title, reg);
 };
-
-//CoverViewer cover track mouse
-CoverViewer.prototype.CoverTrackMouseEvnt = function()
-{
-  //Call the Mouse event
-  CoverViewerCoverTrackMouseEvnt(this);
-}
 
 //CoverViewer Cover track draw
 CoverViewer.prototype.CoverTrackDraw = function(s)
@@ -30,18 +23,8 @@ CoverViewer.prototype.CoverTrackDraw = function(s)
 	//Clear the layer
 	canvas.Clear();
 
-  //Calculate the draw width
-  this.cover.draw.width = this.cover.width - this.cover.draw.margin.left - this.cover.draw.margin.right;
-
-  //Calculate the draw height
-  this.cover.draw.height = this.cover.height - this.cover.draw.margin.top - this.cover.draw.margin.bottom;
-
-  //Show in console
-  //console.log('CoverViewer: Cover Track ' + this.cover.draw.width + 'x' + this.cover.draw.height);
-
   //Check for adjust the data
-  //if(this.core.resized === true || this.cover.data === null)
-  if(this.cover.data === null)
+  if(this.cover.data === null) //if(this.core.resized === true || this.cover.data === null)
   {
     //Adjust the data
     this.cover.data = this.DataGen(this.cover.draw.height);
@@ -53,56 +36,42 @@ CoverViewer.prototype.CoverTrackDraw = function(s)
     console.log('CoverViewer: data successful adjusted to draw window');
   }
 
-  //Save the start position
-  this.cover.start = s;
+  //Save the start index
+  this.cover.index.start = (s < 0) ? 0 : s;
+
+  //Save the end index
+  this.cover.index.end = this.cover.index.start + this.cover.draw.width - 1;
+
+  //Check the end index
+  if(this.data.cover.data.length <= this.cover.index.end)
+  {
+    //Replace the end index
+    this.cover.index.end = this.data.cover.data.length - 1;
+
+    //Update hte start index
+    this.cover.index.start = this.cover.index.end - this.cover.draw.width + 1;
+  }
+
+  //Get the draw start
+  this.cover.draw.start = this.data.cover.data[this.cover.index.start].pos
+
+  //Get the draw end
+  this.cover.draw.end = this.data.cover.data[this.cover.index.end].pos;
 
   //Save the length window
-  this.cover.length = this.cover.draw.width;
-
-  //Check the start position
-  if(s < this.data.cover.data[0].pos)
-  {
-    //Set the start position at the region start
-    this.cover.start = this.data.cover.data[0].pos;
-  }
-
-  //Calculate the end position
-  this.cover.end = this.cover.start + this.cover.length;
-
-  //Check if end > region end
-  if(this.cover.end > this.data.cover.data[this.data.cover.data.length - 1].pos)
-  {
-    //Change the end position
-    this.cover.end = this.data.cover.data[this.data.cover.data.length - 1].pos;
-
-    //Move the start
-    this.cover.start = this.cover.end - this.cover.length;
-
-    //Show error in console
-    console.log('CoverViewer: window end > region end');
-  }
+  this.cover.draw.length = this.cover.draw.end - this.cover.draw.start;
 
   //Show in console
-  //console.log('CoverViewer: drawing [' + this.cover.start + ',' + this.cover.end + ']');
+  //console.log('CoverViewer: drawing [' + this.cover.draw.start + ',' + this.cover.draw.end + ']');
 
   //Find the start position
-  var starti = this.DataFindPos(this.cover.start);
+  //var starti = this.DataFindPos(this.cover.draw.start);
 
   //Find the end position
-  var endi = this.DataFindPos(this.cover.end);
-
-  //Check the starti and endi
-  if(starti < 0 || endi < 0)
-  {
-    //Show error
-    console.error('CoverViewer: start position not found...');
-
-    //Exit
-    return;
-  }
+  //var endi = this.DataFindPos(this.cover.draw.end);
 
   //Draw the control points
-	this.PointsDraw(canvas, this.cover.start, this.cover.end, this.cover.height, this.cover.draw.margin);
+	this.PointsDraw(canvas, this.cover.draw.start, this.cover.draw.end, this.cover.height, this.cover.draw.margin);
 
   //Draw the lines
   for(var j = 0; j < this.bams.num; j++)
@@ -114,13 +83,13 @@ CoverViewer.prototype.CoverTrackDraw = function(s)
     var p = this.cover.draw.margin.left;
 
     //Get the line style
-    var lstyle = { "width": this.cover.stroke, "color": this.bams.color[j] };
+    var lstyle = { 'width': this.cover.stroke, 'color': this.bams.color[j] };
 
 		//Check the line opacity
 		lstyle.opacity = (this.bams.active[j] === false)? this.bams.opacity : 1.0;
 
     //Read all the positions
-    for(var i = starti; i < endi; i++)
+    for(var i = this.cover.index.start; i <= this.cover.index.end; i++)
     {
       //Calculate the next position
       var py = this.cover.height - this.cover.draw.margin.bottom - this.cover.data[i][j];
@@ -164,28 +133,41 @@ CoverViewer.prototype.CoverTrackDrawLabel = function(px)
   //Save the position y
   var posy = this.cover.label.posy;
 
+  //Set the rectanle position x
+  var rect_x = posx - this.cover.label.width/2;
+
+  //Set the rectanlge width
+  var rect_width = this.cover.label.width;
+
+  //Set the rectanlge height
+  var rect_height = this.cover.label.height;
+
+  //Set the rectangle radius
+  var rect_radius = this.cover.label.radius;
+
   //Draw the rectangle
-  canvas.Rect({
-		x: posx - this.cover.label.width/2,
-		y: posy,
-		width: this.cover.label.width,
-		height: this.cover.label.height,
-		radius: this.cover.label.radius
-	});
+  canvas.Rect({ x: rect_x, y: posy, width: rect_width, height: rect_height, radius: rect_radius });
 
   //Set the style
   canvas.Fill(this.cover.label.fill);
 
+  //Save the text
+  var text_text = this.cover.hover.position.toString();
+
+  //Save the text font
+  var text_font = this.cover.label.text.font;
+
+  //Save the text size
+  var text_size = this.cover.label.text.size;
+
+  //Save the text align
+  var text_align = this.cover.label.text.align;
+
+  //Save the text color
+  var text_color = this.cover.label.text.color;
+
   //Show the text
-  canvas.Text({
-		text: this.cover.hover.position.toString(),
-		x: posx,
-		y: posy + 2,
-		font: this.cover.label.text.font,
-		size: this.cover.label.text.size,
-		align: this.cover.label.text.align,
-		color: this.cover.label.text.color
-	});
+  canvas.Text({ text: text_text, x: posx, y: posy + 2, font: text_font, size: text_size, align: text_align, color: text_color });
 
   //Draw the triangle
   canvas.Polygon([[posx - 6, posy + 2],[posx, posy - 6],[posx + 6, posy + 2]]);
@@ -213,7 +195,7 @@ CoverViewer.prototype.CoverTrackDrawHover = function(px, py)
   var posy = py - this.cover.draw.margin.top;
 
   //Save the real position
-  this.cover.hover.position = Math.floor(this.cover.start + posx - this.cover.draw.margin.left);
+  this.cover.hover.position = Math.floor(this.cover.draw.start + posx - this.cover.draw.margin.left);
 
   //Save the index
   this.cover.hover.positioni = this.DataFindPos(this.cover.hover.position);
@@ -271,34 +253,52 @@ CoverViewer.prototype.CoverTrackDrawValue = function(px, py)
       //Save the position y
       var posy = this.cover.hover.circle[i].posy - this.cover.value.height/2;
 
+      //Save the rectangle position x
+      var rect_x = posx - this.cover.value.width;
+
+      //Save the rectanle width
+      var rect_width = this.cover.value.width;
+
+      //Save the rectangle height
+      var rect_height = this.cover.value.height;
+
+      //Save the rectangle radius
+      var rect_radius = this.cover.value.radius;
+
       //Draw the rectangle
-      canvas.Rect({
-				x: posx - this.cover.value.width,
-				y: posy,
-				width: this.cover.value.width,
-				height: this.cover.value.height,
-				radius: this.cover.value.radius,
-			});
+      canvas.Rect({ x: rect_x, y: posy, width: rect_width, height: rect_height, radius: rect_radius });
 
       //Set the style
       canvas.Fill(this.bams.color[i]);
 
       //Create the text
-      var txt = this.data.cover.data[this.cover.hover.positioni].cover[i].toString();
+      var text_text = this.data.cover.data[this.cover.hover.positioni].cover[i].toString();
+
+      //Save the text font
+      var text_font = this.cover.value.text.font;
+
+      //Save the text color
+      var text_color = this.cover.value.text.color;
+
+      //Save the text size
+      var text_size = this.cover.value.text.size;
+
+      //Save the text position x
+      var text_x = posx - this.cover.value.width/2;
+
+      //Save the text position y
+      var text_y = posy - 1;
+
+      //Save the text align
+      var text_align = this.cover.value.text.align;
 
       //Move the text
-      canvas.Text({
-				text: txt,
-				font: this.cover.value.text.font,
-				color: this.cover.value.text.color,
-				size: this.cover.value.text.size,
-				x: posx - this.cover.value.width/2,
-				y: posy - 1,
-				align: this.cover.value.text.align
-			});
+      canvas.Text({ text: text_text, font: text_font, color: text_color, size: text_size, x: text_x, y: text_y, align: text_align });
 
-      //Change the positions for the triangle
+      //Change the position x for the triangle
       posx = posx - 1;
+
+      //Change the position y for the triangle
       posy = this.cover.hover.circle[i].posy;
 
       //Draw the triangle
@@ -307,7 +307,7 @@ CoverViewer.prototype.CoverTrackDrawValue = function(px, py)
       //Set the style
       canvas.Fill(this.bams.color[i]);
 
-      //Exit
+      //Exit the loop
       break;
     }
   }
@@ -320,10 +320,10 @@ CoverViewer.prototype.CoverTrackMouseMove = function(x, y)
   if(this.cover.mouse === true)
   {
     //Calculate the difference
-    var diff = this.cover.click - x;
+    var diff = this.cover.click.position - x;
 
     //Calculate the start point
-    this.preview.window.start = this.cover.clickstart + diff/this.preview.mult;
+    this.preview.window.start = this.cover.click.start + diff/this.preview.draw.scale;
 
     //Draw the region
     this.Move();
@@ -350,7 +350,7 @@ CoverViewer.prototype.CoverTrackMouseDown = function(x)
   this.cover.mouse = true;
 
   //Activate the click first
-  this.cover.clickfirst = true;
+  this.cover.click.first = true;
 
   //Set cursor as move
   $('body').addClass(this.cursor.move);
@@ -359,7 +359,7 @@ CoverViewer.prototype.CoverTrackMouseDown = function(x)
   this.cover.click = x;
 
   //Save the start position
-  this.cover.clickstart = this.preview.window.start;
+  this.cover.click.start = this.preview.window.start;
 
   //Destroy the genes info
   this.GenesTrackInfoClear();
@@ -376,10 +376,10 @@ CoverViewer.prototype.CoverTrackMouseUp = function(x)
 };
 
 //CoverViewer Cover Track mouse function event
-function CoverViewerCoverTrackMouseEvnt(_main)
+function CoverViewerCoverTrackEvents(_this)
 {
   //Save the ID
-  var _id = '#' + _main.cover.id;
+  var _id = '#' + _this.cover.id;
 
   //Mouse up
   $(_id).mouseup(function(e){
@@ -388,7 +388,7 @@ function CoverViewerCoverTrackMouseEvnt(_main)
     e.preventDefault();
 
     //Call the click handler
-    _main.CoverTrackMouseUp(e.pageX - $(this).offset().left);
+    _this.CoverTrackMouseUp(e.pageX - $(this).offset().left);
 
   });
 
@@ -399,7 +399,7 @@ function CoverViewerCoverTrackMouseEvnt(_main)
     e.preventDefault();
 
     //Call the click handler
-    _main.CoverTrackMouseDown(e.pageX - $(this).offset().left);
+    _this.CoverTrackMouseDown(e.pageX - $(this).offset().left);
 
   });
 
@@ -410,7 +410,7 @@ function CoverViewerCoverTrackMouseEvnt(_main)
     e.preventDefault();
 
     //Call the click handler
-    _main.CoverTrackMouseMove(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top);
+    _this.CoverTrackMouseMove(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top);
 
   });
 }
