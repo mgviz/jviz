a017282077ae18a2ef2cf75596140bfd1d5808e5
jmjuanes <josemijuanes@gmail.com>
2016-03-06 13:44:01 +0100

    Update genes import

--- scripts/coverviewer/import/import-genes.js
+++ scripts/coverviewer/import/import-genes.js
@@ -9,7 +9,7 @@ CoverViewer.prototype.ImportDataGenes = function(region)
   url = url.replace(/{ region }/gi, region); //Replace for region with spaces
 
   //Show in console
-  console.log('CoverViewer: reading genes data from "' + url + '"');
+  console.log('CoverViewer: reading genes from "' + url + '"');
 
   //Set genes as busy
   this.data.genes.busy = true;
@@ -41,74 +41,40 @@ CoverViewer.prototype.ImportDataGenesError = function(url)
 CoverViewer.prototype.ImportDataGenesParser = function(data)
 {
   //Save the data
-  this.data.genes.data = data;
+  this.data.genes.list = data;
 
   //Check parser
   if(this.data.genes.parser)
   {
     //Parse the data
-    this.data.genes.data = this.data.genes.parser(data);
+    this.data.genes.list = this.data.genes.parser(data);
   }
 
-  //Sort the genes data
-  this.data.genes.data.genes = ObjectSort(this.data.genes.data.genes, 'start');
-
-  //Filter the genes by region
-  this.ImportDataGenesFilter();
+  //Sort the genes list
+  this.data.genes.list = ObjectSort(this.data.genes.list, 'start');
 
   //Calculate the genes track height
   this.GenesTrackHeight();
 
-  //Filter the strand
-  this.GenesTrackStrandCheck();
+  //Show the specie info
+  this.GenesTrackBarSpecieInfo();
 
   //Set genes busy as false
   this.data.genes.busy = false;
-
-  //Check for get the exons
-  if(this.data.exons.use === true)
-  {
-    //Import exons
-    this.ImportDataExons();
-  }
 
   //Continue
   this.DrawReady();
 };
 
-//CoverViewer filter the genes list to the region
-CoverViewer.prototype.ImportDataGenesFilter = function()
-{
-  //New array
-  var ngenes = [];
-
-  //Read all selected genes
-  for(var i = 0; i < this.data.genes.data.genes.length; i++)
-  {
-    //Get the gene
-    var ge = this.data.genes.data.genes[i];
-
-    //Check the start and end positions
-    if(ge.start <= this.draw.end && this.draw.start <= ge.end)
-    {
-      //Add the gene
-      ngenes.push(ge);
-    }
-  }
-
-  //Save the genes list
-  this.data.genes.data.genes = ngenes;
-};
-
 //Function for import the data with jquery
-function CoverViewerImportDataGenesJson(_url, _main)
+function CoverViewerImportDataGenesJson(_url, _this)
 {
   //Create the conection
   var _import = $.ajax({url: _url, dataType: 'json'});
 
   //Done function
-  _import.done(function(data){ _main.ImportDataGenesParser(data); });
+  _import.done(function(data){ _this.ImportDataGenesParser(data); });
 
   //Fail function
-  _import.fail(function(){ _main.ImportDataGenesError(_url); });
+  _import.fail(function(){ _this.ImportDataGenesError(_url); });
 }
