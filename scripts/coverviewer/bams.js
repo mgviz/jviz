cdf114c8b2551b4f8460c6a8502e6ea50c481d4b
jmjuanes <josemijuanes@gmail.com>
2016-03-12 14:32:12 +0100

    Add empty bam class

--- scripts/coverviewer/bams.js
+++ scripts/coverviewer/bams.js
@@ -1,28 +1,21 @@
 //CoverViewer Preview Stroke
 CoverViewer.prototype.BamsParser = function()
 {
-  //Save the number of bams
-  this.bams.num = this.data.cover.data[0].cover.length;
-
-  //Restart the colors array
-  this.bams.color = [];
-
-  //Save the color
-  for(var i = 0; i < this.bams.num; i++)
+  //For get hte number of bams
+  for(var key in this.data.cover.data)
   {
-    //Default color
-    var c = this.ColorByIndex(i);
+    //Save the number of bams
+    this.bams.num = this.data.cover.data[key].length;
 
-    //Check for custom color
-    if(this.data.cover.color)
-    {
-      //Call to the custom function
-      c = this.data.cover.color(i);
-    }
-
-    //Push the color
-    this.bams.color.push(c);
+    //Exit
+    break;
   }
+
+  //Initialize the empty bams
+  this.BamsEmptyInit();
+
+  //Initialize the bams color
+  this.BamsColorInit();
 
   //Initialize the bams labels
   this.BamsLabelsInit();
@@ -62,4 +55,35 @@ CoverViewer.prototype.BamsActiveInit = function()
     //Insert true
     this.bams.active.push(true);
   }
+};
+
+//CoverViewer parse the bams color
+CoverViewer.prototype.BamsColorInit = function()
+{
+  //Restart the colors array
+  this.bams.color = [];
+
+  //Save the color
+  for(var i = 0; i < this.bams.num; i++)
+  {
+    //Default color
+    var c = this.ColorByIndex(i);
+
+    //Check for custom color
+    if(this.data.cover.color)
+    {
+      //Call to the custom function
+      c = this.data.cover.color(i);
+    }
+
+    //Push the color
+    this.bams.color.push(c);
+  }
+};
+
+//CoverViewer Initialize the empty bam cover
+CoverViewer.prototype.BamsEmptyInit = function()
+{
+  //Reset the empty bam cover
+  this.bams.empty = jvizMath.Zeros(this.bams.num);
 };
