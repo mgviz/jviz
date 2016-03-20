fe2ffdcc159c59913dd02e5b113a0020ed87df62
jmjuanes <josemijuanes@gmail.com>
2016-03-20 17:44:30 +0100

    Add options for import karyotypes

--- scripts/coverviewer/options.js
+++ scripts/coverviewer/options.js
@@ -1,17 +1,20 @@
 //CoverViewer Options function
 CoverViewer.prototype.Options = function(opt)
 {
+  //For check the specie
+  if(typeof opt.specie !== 'undefined'){ this.data.specie = opt.specie; }
+  
+  //For check the assembly
+  if(typeof opt.assembly !== 'undefined'){ this.data.assembly = opt.assembly; }
+
   //For test mode
   if(typeof opt.testMode !== 'undefined'){ this.default.test = opt.testMode; }
 
-  //Check for show button settings
-  if(typeof opt.showBtnSettings !== 'undefined'){ this.navbar.btnSettingsShow = opt.showBtnSettings; }
+  //Check for show button labes
+  if(typeof opt.showBtnLabels !== 'undefined'){ this.navbar.btn.labels.show = opt.showBtnLabels; }
 
   //Check for show the preview track
   if(typeof opt.showTrackPreview !== 'undefined') { this.preview.show = opt.showTrackPreview; }
-
-  //Check for show the names track
-  if(typeof opt.showTrackNames !== 'undefined') { this.names.show = opt.showTrackNames; }
 
   //Check for show the cover track
   if(typeof opt.showTrackCover !== 'undefined'){ this.cover.show = opt.opt.showTrackCover; }
@@ -22,7 +25,18 @@ CoverViewer.prototype.Options = function(opt)
   //Check for gaus filter times
   if(typeof opt.gaussFilter !== 'undefined'){ this.gauss.times = opt.gaussFilter; }
 
-  //Check for fix cover gaps
-  if(typeof opt.coverFixGaps !== 'undefined'){ this.data.cover.fixgaps = opt.coverFixGaps; }
+  //Check for bam names
+  if(typeof opt.bamNames !== 'undefined'){ this.bams.labels = opt.bamNames; }
 
+  //Check for import names
+  if(typeof opt.importNames !== 'undefined'){ this.ImportNames(opt.importNames); }
+
+  //Check for import karyotypes
+  if(typeof opt.importKaryotypes !== 'undefined'){ this.ImportKaryotypes(opt.importKaryotypes); }
+
+  //Check for import cover
+  if(typeof opt.importCover !== 'undefined'){ this.ImportCover(opt.importCover); }
+
+  //Check for import genes
+  if(typeof opt.importGenes !== 'undefined'){ this.ImportGenes(opt.importGenes); }
 };
