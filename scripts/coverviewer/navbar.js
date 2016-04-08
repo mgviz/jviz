d7f5623c1290c2714ff9596bb61e01405db02efa
jmjuanes <josemijuanes@gmail.com>
2016-04-08 19:01:30 +0200

    Add new buttons events

--- scripts/coverviewer/navbar.js
+++ scripts/coverviewer/navbar.js
@@ -1,80 +1,110 @@
-//CoverViewer Navbar Builder
-CoverViewer.prototype.NavbarBuild = function()
-{
-  //Create the new navbar element
-  var div = '<div id="' + this.navbar.id + '" class="' + this.navbar.class + '" ';
-
-  //Check if navbar is visible
-  if(this.navbar.show === false)
-  {
-    //Add the display as none
-    div = div + 'style="display:none;"'
-  }
-
-  //End the div
-  div = div + '>';
-
-  //Create the left button
-  div = div + this.NavbarBuildBtn(this.navbar.btn.left);
-
-  //Create the right button
-  div = div + this.NavbarBuildBtn(this.navbar.btn.right);
-
-  //Create the labels button
-  div = div + this.NavbarBuildBtn(this.navbar.btn.labels);
-
-  //Close the navbar div
-  div = div + '</div>';
-
-  //Return the new element
-  return div;
-};
-
-//CoverViewer navbar buttons build
-CoverViewer.prototype.NavbarBuildBtn = function(obj)
-{
-  //Check for show
-  if(obj.show === false){ return ''; }
-
-  //Return the button
-  return '<div id="' + obj.id + '" class="' + obj.class + '" title="' + obj.title + '"></div>';
-};
-
-//CoverViewer Settings button click
+//CoverViewer Labels button click
 CoverViewer.prototype.NavbarBtnLabelsClick = function()
 {
   //Open the labels dialog
   this.LabelsOpen();
 };
 
-//CoverViewer Arrow events
-CoverViewer.prototype.NavbarBtnArrowClick = function(btn)
+//Coverviewer karyotypes button click
+CoverViewer.prototype.NavbarKaryotypesClick = function()
 {
-  //Initialize the increment
-  var inc = this.prevwindow.width;
+  //Change the status
+  this.draw.status = 'karyotypes';
+  
+  //Show loading
+  this.preview.LoadingShow();
 
-  //Check if the button press is left
-  if(btn === 'l')
+  //Draw
+  CoverViewerDraw(this);
+};
+
+//CoverViewer Navbar Input event
+CoverViewer.prototype.NavbarInputClick = function()
+{
+	//Get the region
+	var region = $('#' + this.navbar.input.id).val();
+
+  //Check for null
+  if(region === '' || region === ' ')
   {
-    //Move to left
-    inc = -inc;
+    //Show error
+    this.alert.Error({ text: this.navbar.input.alert.text, time: this.navbar.input.alert.time });
+
+    //Exit
+    return;
   }
 
-  //Calculate the new start point
-  this.prevwindow.start = this.prevwindow.start + inc;
+	//Covert to lower case
+  region = region.toLowerCase();
 
-  //Draw the region
-  this.Move();
+  //Find region index
+  var index = -1;
+
+  //Search in all the regions
+  for(var i = 0; i < this.data.regions.data.length; i++)
+  {
+    //Get the region
+    var r = this.data.regions.data[i];
+
+    //Check the region ID
+    if(typeof r.id !== 'undefined')
+    {
+      //Check for region id
+      if(region === r.id.toLowerCase()){ index = i; break; }
+    }
+
+    //Check the region name
+    if(typeof r.name !== 'undefined')
+    {
+      //Check for region name
+      if(region === r.name.toLowerCase()){ index = i; break; }
+    }
+
+    //Check the region label
+    if(typeof r.label !== 'undefined')
+    {
+      //Check for region label
+      if(region === r.label.toLowerCase()){ index = i; break; }
+    }
+  }
+
+  //Check for null
+  if(index < 0)
+  {
+    //Show error
+    this.alert.Error({ text: this.navbar.input.alert.text, time: this.navbar.input.alert.time });
+
+    //Exit
+    return;
+  }
+
+  //Get the region
+  var r = jvizRegion.Join(this.data.regions.data[index]);
+
+  //Show preview loading
+	this.preview.LoadingShow();
+
+	//Show cover loading
+	this.cover.LoadingShow();
+
+	//Show genes loading
+	this.genes.LoadingShow();
+
+  //Open the region
+  CoverViewerGoTo(this, r);
 };
 
 //Navbar Buttons event
 function CoverViewerNavbarBtnEvents(_this)
 {
-  //Left button
-  $('#' + _this.navbar.btn.left.id).click(function(e){ _this.NavbarBtnArrowClick('l'); });
+  //Go back to karyotypes
+  $('#' + _this.navbar.btn.karyotypes.id).click(function(e){ _this.NavbarKaryotypesClick(); });
 
-  //Right button
-  $('#' + _this.navbar.btn.right.id).click(function(e){ _this.NavbarBtnArrowClick('r'); });
+  //Input region
+	$('#' + _this.navbar.input.id).keyup(function(e){ if(e.keyCode == 13){ _this.NavbarInputClick(); } });
+
+  //Find region
+  $('#' + _this.navbar.btn.search.id).click(function(e){ _this.NavbarInputClick(); });
 
   //Labels button
   $('#' + _this.navbar.btn.labels.id).click(function(e){ _this.NavbarBtnLabelsClick(); });
