4d3519ef45b107a5952d5b10b55cdc5150bd31aa
jmjuanes <josemijuanes@gmail.com>
2016-05-08 22:17:59 +0200

    Create the karyodb adapter

--- scripts/adapters/karyodb.js
+++ scripts/adapters/karyodb.js
@@ -1,5 +1,55 @@
 //jvizKaryoDBAdapter
-var jvizKaryoDBAdapter = function()
+var jvizKaryoDBAdapter = function(mirror)
 {
+	//Default link
+	var url = 'https://' + 'raw.githubusercontent.com/biowt/karyojs-species/v1';
 
+	//Save the mirror
+	this.mirror = (typeof mirror === 'undefined') ? url : mirror;
+
+	//Parse the mirror
+	if(this.mirror.charAt(this.mirror.length - 1) === '/')
+	{
+		//Remove the last character
+		this.mirror = this.mirror.substring(0, this.mirror.length - 1);
+	}
+
+	//Base url for data
+	this.data = '/{specie}/{assembly}.json';
+
+	//Return the adapter
+	return this;
+};
+
+//jvizKaryoDBAdapter generate the url
+jvizKaryoDBAdapter.prototype.Url = function(opt)
+{
+	//Check the dataset
+	if(typeof opt === 'undefined'){ var opt = {}; }
+
+	//Generate the url
+	var url = this.mirror + this.data;
+
+	//Replace the options
+	url = jvizRepStr(url, opt, { prefix: '{', suffix: '}' });
+
+	//Return the url
+	return url;
+};
+
+//jvizKaryoDBAdapter parse the result
+jvizKaryoDBAdapter.prototype.Parse = function(data)
+{
+	//Return the chromosomes list
+	return data.chr;
+};
+
+//jvizKaryoDBAdapter get data
+jvizKaryoDBAdapter.prototype.Get = function(opt)
+{
+	//Get the json file
+	var data = jvizGetJSON(this.Url(opt));
+
+	//Parse the data and return
+	return this.Parse(data);
 };
