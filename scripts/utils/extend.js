7d4ac9d71820f7e88b7434948b0483c65a684aa3
jmjuanes <josemijuanes@gmail.com>
2016-05-22 12:20:55 +0200

    Check undefined keys

--- scripts/utils/extend.js
+++ scripts/utils/extend.js
@@ -1,6 +1,9 @@
 //jvizExtend: extend an object with the keys in another object
 function jvizExtend(original, extend, keys)
 {
+	//Check for undefined keys
+	if(typeof keys === 'undefined'){ var keys = Object.keys(extend); }
+	
 	//Read the full list
 	for(var i = 0; i < keys.length; i++)
 	{
