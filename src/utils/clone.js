496bcaafc525e25c3a927983204616190ac2719d
jmjuanes <josemijuanes@gmail.com>
2016-07-16 19:09:14 +0200

    src/utils/clone.js: minor changes

--- src/utils/clone.js
+++ src/utils/clone.js
@@ -1,12 +1,12 @@
 //Function for cloning an object
 jviz.utils.clone = function(obj)
 {
-	//Convert the object to string
-	var o = JSON.stringify(obj);
+  //Convert the object to string
+  var o = JSON.stringify(obj);
 
-	//Convert again to json
-	o = JSON.parse(o);
+  //Convert again to json
+  o = JSON.parse(o);
 
-	//Return the object
-	return o;
+  //Return the object
+  return o;
 };
