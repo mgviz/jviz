70c7c3090a586a6fbbe5ee9a9f491ad9bc25cacd
jmjuanes <josemijuanes@gmail.com>
2016-07-16 19:11:16 +0200

    src/utils/hash.js: minor changes

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -4,19 +4,19 @@ var jviz.utils.hash = {};
 //Get hash
 jviz.utils.hash.get = function()
 {
-	//Get the hash
-	var h = window.location.hash.substr(1);
+  //Get the hash
+  var h = window.location.hash.substr(1);
 
-	//Return the decoded uri
-	return decodeURIComponent(h);
+  //Return the decoded uri
+  return decodeURIComponent(h);
 };
 
 //Set hash
 jviz.utils.hash.set = function(h)
 {
-	//Encode the hash value
-	h = encodeURIComponent(h);
+  //Encode the hash value
+  h = encodeURIComponent(h);
 
-	//Set the hash
-	window.location.hash = '#' + h;
+  //Set the hash
+  window.location.hash = '#' + h;
 };
