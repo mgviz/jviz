d9bfc6fd05441a875ea352b1e3bbb1f9838db021
jmjuanes <josemijuanes@gmail.com>
2016-07-16 19:09:50 +0200

    src/utils/get-id.js: minor changes

--- src/utils/get-id.js
+++ src/utils/get-id.js
@@ -2,101 +2,101 @@
 //Extracted from https://github.com/jmjuanes/getid/
 jviz.utils.getID = function(opt)
 {
-	//Characters available
-	var _chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  //Characters available
+  var _chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-	//Initialize the exists array
-	var _exists = [];
+  //Initialize the exists array
+  var _exists = [];
 
-	//Initialize the ID length
-	var _length = 22;
+  //Initialize the ID length
+  var _length = 22;
 
-	//Initialize the prefix
-	var _prefix = '';
+  //Initialize the prefix
+  var _prefix = '';
 
-	//Initialize the suffix
-	var _suffix = '';
+  //Initialize the suffix
+  var _suffix = '';
 
-	//Check the user options
-	if(typeof opt !== 'undefined')
-	{
-		//Check the existen IDs
-		if(typeof opt.exists !== 'undefined'){ _exists = opt.exists; }
+  //Check the user options
+  if(typeof opt !== 'undefined')
+  {
+  //Check the existen IDs
+  if(typeof opt.exists !== 'undefined'){ _exists = opt.exists; }
 
-		//Check the ID length
-		if(typeof opt.length !== 'undefined'){ _length = parseInt(opt.length); }
+  //Check the ID length
+  if(typeof opt.length !== 'undefined'){ _length = parseInt(opt.length); }
 
-		//Check the prefix option
-		if(typeof opt.prefix !== 'undefined'){ _prefix = opt.prefix; }
+  //Check the prefix option
+  if(typeof opt.prefix !== 'undefined'){ _prefix = opt.prefix; }
 
-		//Check the suffix option
-		if(typeof opt.suffix !== 'undefined'){ _suffix = opt.suffix ; }
-	}
+  //Check the suffix option
+  if(typeof opt.suffix !== 'undefined'){ _suffix = opt.suffix ; }
+  }
 
-	//Generated id
-	var id = '';
+  //Generated id
+  var id = '';
 
-	//Build the new ID
-	do
-	{
-		//Get the time
-		id = Date.now().toString();
+  //Build the new ID
+  do
+  {
+    //Get the time
+    id = Date.now().toString();
 
-		//Get a random number of insertions
-		var ins = jviz.math.randInt(4, 10);
+    //Get a random number of insertions
+    var ins = jviz.math.randInt(4, 10);
 
-		//Check the length
-		if(_length > 0)
-		{
-			//Check if the actual ID exceeds the length
-			if(id.length > _length)
-			{
-				//Calculate the number of insertions in function the length
-				ins = (_length > 4)? 2 : 1;
+    //Check the length
+    if(_length > 0)
+    {
+      //Check if the actual ID exceeds the length
+      if(id.length > _length)
+      {
+        //Calculate the number of insertions in function the length
+        ins = (_length > 4)? 2 : 1;
 
-				//Get a new random integer
-				var p = jviz.math.randInt(0, id.length - _length - 1);
+        //Get a new random integer
+        var p = jviz.math.randInt(0, id.length - _length - 1);
 
-				//Slice the actual ID
-				id = id.slice(p, p + _length - ins);
-			}
-			else if(id.length + ins > _length)
-			{
-				//Change the number of insertions
-				ins = _length - id.length;
-			}
-			else if(id.length + ins < _length)
-			{
-				//Change the number of insertions
-				ins = _length - id.length;
-			}
-		}
+        //Slice the actual ID
+        id = id.slice(p, p + _length - ins);
+      }
+      else if(id.length + ins > _length)
+      {
+        //Change the number of insertions
+        ins = _length - id.length;
+      }
+      else if(id.length + ins < _length)
+      {
+        //Change the number of insertions
+        ins = _length - id.length;
+      }
+    }
 
-		//Add the insertions
-		for(var i = 0; i < ins; i++)
-		{
-			//Get a random character
-			var ch = _chars[jviz.math.randInt(0, _chars.length - 1)];
+    //Add the insertions
+    for(var i = 0; i < ins; i++)
+    {
+      //Get a random character
+      var ch = _chars[jviz.math.randInt(0, _chars.length - 1)];
 
-			//Get the new position for insert
-			var po = jviz.math.randInt(0, id.length - 1);
+      //Get the new position for insert
+      var po = jviz.math.randInt(0, id.length - 1);
 
-			//Insert
-			id = id.slice(0, po) + ch + id.slice(po);
-		}
+      //Insert
+      id = id.slice(0, po) + ch + id.slice(po);
+    }
 
-		//Add the prefix and the suffix
-		id = _prefix + id + _suffix;
-	}
-	while(_exists.indexOf(id) > -1);
+    //Add the prefix and the suffix
+    id = _prefix + id + _suffix;
+  }
+  while(_exists.indexOf(id) > -1);
 
-	//Return the new ID
-	return id;
-}
+  //Return the new ID
+  return id;
+};
 
 //Alias for GetID
 jviz.utils.genID = function(opt)
 {
-	//Generate an ID
-	return jviz.utils.getID(opt);
+  //Generate an ID
+  return jviz.utils.getID(opt);
 };
