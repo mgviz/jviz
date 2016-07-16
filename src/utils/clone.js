5cbf4c90204cd387c36337b9bda889b5d3d391bc
jmjuanes <josemijuanes@gmail.com>
2016-07-16 18:34:43 +0200

    src/utils/clone.js: created method to clone objects

---
+++ src/utils/clone.js
@@ -0,0 +1,12 @@
+//Function for cloning an object
+jviz.utils.clone = function(obj)
+{
+	//Convert the object to string
+	var o = JSON.stringify(obj);
+
+	//Convert again to json
+	o = JSON.parse(o);
+
+	//Return the object
+	return o;
+};
