8d6a20f47f723099baa16711844f0ed0108e29ef
jmjuanes <josemijuanes@gmail.com>
2016-07-17 16:11:51 +0200

    src/ui/tooltip.js: updated tooltip

--- src/ui/tooltip.js
+++ src/ui/tooltip.js
@@ -5,7 +5,7 @@ jviz.ui.toolTip = function(opt)
   if(typeof opt === 'undefined'){ var opt = {}; }
 
   //Save the ID
-  this.id = (typeof opt.id === 'undefined') ? jviz.utils.getID({ prefix: 'jviz-tooltip', length: 5 }) : opt.id;
+  this.id = (typeof opt.id === 'undefined') ? jviz.utils.getID({ prefix: 'jviz-tooltip-', length: 5 }) : opt.id;
 
   //Save the class
   this.class = (typeof opt.class === 'undefined') ? 'jviz-ui-tooltip' : opt.class;
@@ -41,29 +41,23 @@ jviz.ui.toolTip = function(opt)
 jviz.ui.toolTip.prototype.build = function(parent)
 {
   //Create the new div
-  var div = '<div id="' + this.id + '" class="' + this.class + '">';
+  jviz.dom.append({ id: this.id, class: this.class }, parent);
 
   //Add the triangle div
-  div = div + '<div id="' + this.triangle.id + '" class="' + this.triangle.class + '"></div>';
+  jviz.dom.append({ id: this.triangle.id, class: this.triangle.class }, this.id);
 
   //Add the content div
-  div = div + '<div id="' + this.content.id + '" class="' + this.content.class + '"></div>';
-
-  //Close the div
-  div = div + '</div>';
-
-  //Save
-  jviz.dom(parent).append(div);
+  jviz.dom.append({ id: this.content.id, class: this.content.class }, this.id);
 
   //Set display as none
-  jviz.dom(this.id).css('display', 'none');
+  $('#' + this.id).css('display', 'none');
 };
 
 //jvizToolTip Set content
 jviz.ui.toolTip.prototype.setContent = function(content)
 {
   //Set the content
-  jviz.dom(this.content.id).html(content);
+  $('#' + this.content.id).html(content);
 };
 
 //jvizToolTip set position
@@ -87,24 +81,24 @@ jviz.ui.toolTip.prototype.setWidth = function(w)
 jviz.ui.toolTip.prototype.show = function()
 {
   //Set the position x
-  jviz.dom(this.id).css('top', this.posy + this.margin.top);
+  $('#' + this.id).css('top', this.posy + this.margin.top);
 
   //Set the position y
-  jviz.dom(this.id).css('left', this.posx - this.width/2);
+  $('#' + this.id).css('left', this.posx - this.width/2);
 
   //Set the width
-  jviz.dom(this.id).width(this.width);
+  $('#' + this.id).width(this.width);
 
   //Update the triangle position left
-  jviz.dom(this.triangle.id).css('left', this.width/2 - this.triangle.width/2);
+  $('#' + this.triangle.id).css('left', this.width/2 - this.triangle.width/2);
 
   //Show the tooltip
-  jviz.dom(this.id).css('display', 'block');
+  $('#' + this.id).css('display', 'block');
 };
 
 //jvizToolTip Hide
 jviz.ui.toolTip.prototype.hide = function()
 {
   //Hide the tooltip
-  jviz.dom(this.id).css('display', 'none');
+  $('#' + this.id).css('display', 'none');
 };
