1130d02ceb8c8d679018a4ed3c626626488cb5ef
jmjuanes <josemijuanes@gmail.com>
2016-08-16 18:36:13 +0200

    src/events.js: manage events

---
+++ src/events.js
@@ -0,0 +1,61 @@
+//Events
+jviz.extend('events', { list: [] });
+
+//Add a new event
+jviz.events.add = function(id, callback, scope)
+{
+  //Check the event callback
+  if(typeof callback !== 'function'){ return console.error('ERROR: callback must be a function...'); }
+
+  //Check the event ID
+  if(typeof id !== 'string'){ return console.error('ERROR: event ID must be a string...'); }
+
+  //Check for empty string
+  if(id.replace(/\s/g, '') === ''){ return console.error('ERROR: event ID must not be an empty string...'); }
+
+  //Parse the id
+  id = id.trim();
+
+  //Check the events list
+  if(typeof this.list[id] === 'undefined'){ this.list[id] = []; }
+
+  //Add the event
+  this.list[id].push({ id: id, scope: scope, callback: callback });
+};
+
+//Send the events
+jviz.extend.send = function(id)
+{
+  //Check the id
+  if(typeof id !== 'string')
+  {
+    //Exit with error
+    return console.error('ERROR: you must specify the ID of the events that you are going to call...');
+  }
+
+  //Parse the id
+  id = id.trim();
+
+  //Check the events
+  if(typeof this.list[id] === 'undefined'){ return; }
+
+  //Arguments list
+  var args = [];
+
+  //Read all the call arguments
+  for(var i = 1; i < arguments.length; i++)
+  {
+    //Add the argument
+    args.push(arguments[i]);
+  }
+
+  //Get the list with all the events
+  var events = this.list[id];
+
+  //Call all the events
+  for(var i = 0; i < events.length; i++)
+  {
+    //Call this event
+    events[i].callback.apply(events[i].scope, args);
+  }
+};
