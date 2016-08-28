f0892b8055bb4f5c019fc1885670a8280dc8ce20
jmjuanes <josemijuanes@gmail.com>
2016-08-28 17:16:04 +0200

    src/components/checkbox.js: created checkbox component

---
+++ src/components/checkbox.js
@@ -0,0 +1,64 @@
+//Checkbox component
+jviz.components.checkbox = function(opt)
+{
+  //Check the options
+  if(typeof opt === 'undefined'){ var opt = {}; }
+
+  //Save the new checkbox id
+  this.id = (typeof opt.id === 'undefined') ? jviz.utils.getID({ preffix: 'checkbox', length: 5 }) : opt.id;
+
+  //Save the checkbox class
+  this.class = (typeof opt.class === 'undefined') ? 'jviz-components-checkbox' : opt.class;
+
+  //Save the parent
+  this.parent = (typeof opt.parent === 'undefined') ? undefined : opt.parent;
+
+  //Checkbox object
+  this.checkbox = {};
+  this.checkbox.id = this.id + '-checkbox'; //Checkbox id
+  this.checkbox.default = (typeof opt.checked === 'undefined') ? false : opt.checked; //Default status
+
+  //Label object
+  this.label = {};
+  this.label.id = this.id + '-label'; //Label id
+
+  //Build the checkbox
+  this.build();
+
+  //Return this
+  return this;
+};
+
+//Build the checkbox
+jviz.components.checkbox.prototype.build = function()
+{
+  //<div class="jviz-components-checkbox" id="checkboxDiv">
+  //	<input type="checkbox" value="1" id="myCheckbox" name="" />
+  //	<label for="myCheckbox"></label>
+  //</div>
+
+  //Create the parent div
+  jviz.dom.append({ type: 'div', id: this.id, class: this.class }, this.parent);
+
+  //Add the input
+  jviz.dom.append({ type: 'input', id: this.checkbox.id, value: 0, name: '' }, this.id);
+
+  //Add the label
+  jviz.dom.append({ type: 'label', id: this.label.id, for: this.checkbox.id }, this.id);
+
+  //Set checked
+  this.checked(this.checkbox.default);
+};
+
+//Set if checkbox is checked
+jviz.components.checkbox.prototype.checked = function(status)
+{
+  //Get the element
+  var el = document.getElementById(this.checkbox.id);
+
+  //Check for status
+  if(typeof status !== 'boolean'){ return el.checked; }
+
+  //Set the checkbox status
+  el.checked = status;
+};
